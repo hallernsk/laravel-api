660ae394292f4e0a0c8faa135c7127f73c70e2db
hallernsk <avladim@mail.ru>
2024-05-24 14:52:50 +0700

    exec migrations

--- database/migrations/2024_05_24_055135_create_desk_lists_table.php
+++ database/migrations/2024_05_24_055135_create_desk_lists_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,9 @@ return new class extends Migration
     {
         Schema::create('desk_lists', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('desk_id')->constrained();
+            // $table->foreign('desk_id')->constrained('desks')->onDelete('cascade');
             $table->timestamps();
         });
     }
